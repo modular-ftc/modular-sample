bb9d26a135b0e7ca532c4baa1387097a87fed1f6
Mitchell Skaggs <skaggsm333@gmail.com>
2017-11-03 16:55:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# modular-sample
+This is a sample repository that uses the Modular FTC dependencies.
---
+++ TeamCode/src/main/java/org/modularftc/sample/JavaTestOpMode.java
@@ -0,0 +1,35 @@
+package org.modularftc.sample;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableGamepad;
+
+import static org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData.Button.A;
+import static org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableButton.ButtonState.JUST_PRESSED;
+import static org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableButton.ButtonState.JUST_RELEASED;
+
+import static org.pattonvillerobotics.commoncode.opmodes.OpModeGroups.TESTING;
+
+/**
+ * Created by Mitchell Skaggs on 10/29/2017.
+ */
+
+@TeleOp(name = "Test OpMode Java", group = TESTING)
+public class JavaTestOpMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        ListenableGamepad gamepad = new ListenableGamepad();
+
+        gamepad.addButtonListener(A, JUST_PRESSED, () -> telemetry.addData("ButtonEvent", "\"A\" Pressed!"))
+                .addButtonListener(A, JUST_RELEASED, () -> telemetry.addData("ButtonEvent", "\"A\" Released!"));
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            gamepad.update(gamepad1);
+            telemetry.update();
+        }
+    }
+}
